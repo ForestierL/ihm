7f42025f4b6d2753b0b682b4989679cbf0238966
Edouard Pigot <edouard.pigot@etu.univ-amu.fr>
2020-01-21 11:52:04 +0100

    Status Bar

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setStatusBar();
     w.show();
     return a.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QLabel>
+#include <QPushButton>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,3 +15,46 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::setStatusBar(){
+    QFrame *statusFrame = new QFrame();
+    statusFrame->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    statusFrame->setStyleSheet("background-color: rgb(255, 0, 0);"
+                               "border: 0px;"
+                               "margin: 0px;"
+                               "padding: 0px;");
+
+    QHBoxLayout *layout = new QHBoxLayout(statusFrame);
+    layout->setContentsMargins(0,0,0,0);
+    layout->setSpacing(0);
+
+    QLabel *statusMessage = new QLabel("0 élement selectionné", statusFrame);
+    statusMessage->setStyleSheet("color: white");
+
+    QFrame *frame = new QFrame(statusFrame);
+    frame->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    frame->setStyleSheet("background-color: rgb(255, 255, 255);"
+                         "border: 0px;"
+                         "margin: 0px;"
+                         "padding: 0px;");
+
+    QPushButton *liste = new QPushButton("Liste", statusFrame);
+    liste->setStyleSheet("background-color: rgb(51, 51, 51);"
+                         "color: rgb(140, 140, 140);"
+                         "border: 0px;"
+                         "margin: 0px;"
+                         "padding: 0px;");
+    QPushButton *icone = new QPushButton("Icone", statusFrame);
+    icone->setStyleSheet("background-color: rgb(51, 51, 51);"
+                         "color: rgb(140, 140, 140);"
+                         "border: 0px;"
+                         "margin: 0px;"
+                         "padding: 0px;");
+
+    layout->addWidget(statusMessage);
+    layout->addWidget(frame);
+    layout->addWidget(liste);
+    layout->addWidget(icone);
+
+    ui->statusbar->addWidget(statusFrame, 1);
+    ui->statusbar->setStyleSheet("background-color: rgb(0,255,0);");
+}
